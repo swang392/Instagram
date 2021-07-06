fce78da118a443c21b6f1509f66bed251185df72
Sarah Wang <sarah.wang2009@gmail.com>
2021-07-06 15:48:45 -0400

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Sarah Wang on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
